81d5ae3a3db7a96ab49064ab5d4e64cba8505f37
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-01-13 18:21:43 +0100

    Added color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,33 @@
 module.exports = {
+	mode: "jit",
+	content: [
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./modules/**/*.{js,ts,jsx,tsx}",
+	],
 	theme: {
 		extend: {
 			fontFamily: {
 				main: ['"main"', "sans-serif"],
 			},
+			colors: {
+				main: "#4b3566",
+				second: "#cabcdc",
+				math_main: "#6B69B8",
+				math_second: "#C7C6F3",
+				croatian_main: "#B85151",
+				croatian_second: "#E2BABA",
+				science_main: "#61845C",
+				science_second: "#BBE1B4",
+				history_main: "#B18E48",
+				history_second: "#B18E48",
+			},
 		},
 	},
 	plugins: [],
 	purge: [
+		"./src/**/*.{js,ts,jsx,tsx}",
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
 		"./modules/**/*.{js,ts,jsx,tsx}",
