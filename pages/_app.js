429cf63ec19caad03233d54b6dca81fcd81a487f
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-01-13 18:23:36 +0100

    Set up twin macro and styled components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
-const MyApp = ({ Component, pageProps }) => {
-	return <Component className="font-main" {...pageProps} />;
-};
+function MyApp({ Component, pageProps }) {
+	return (
+		<>
+			<Head>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
+}
 
 export default MyApp;
