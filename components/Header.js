d91125217ba0af95e83358dd49490d0f4b1dbbdc
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-01-14 16:55:11 +0100

    Refactored Header component

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,50 @@
 import React, { useState, useEffect } from "react";
 import tw, { styled } from "twin.macro";
-import { setBackgroundColor, setHighlightColor } from "../utils/color";
+import { BackgroundColor, HighlightColor } from "../utils/color";
 
-const Header = ({ color = "main" }) => {
+const Header = ({ color = "main", isLogged = false }) => {
 	const [primary, setPrimary] = useState();
 	const [secondary, setSecondary] = useState();
 
 	useEffect(() => {
-		setPrimary(setBackgroundColor(color));
-		setSecondary(setHighlightColor(color));
+		setPrimary(BackgroundColor[color]);
+		setSecondary(HighlightColor[color]);
 	}, [color]);
 
-	const HeaderStyle = styled.header(() => [primary, tw`h-24`]);
 	return (
 		<>
-			<HeaderStyle />
+			<HeaderStyle color={primary}>
+				<StyledNav
+					color={secondary}
+					rest={tw`rounded-2xl w-32`}
+				></StyledNav>
+				<section className="flex justify-end w-full">
+					{isLogged && (
+						<StyledNav
+							color={secondary}
+							rest={tw`rounded-full mx-0`}
+						></StyledNav>
+					)}
+					<StyledNav
+						color={secondary}
+						rest={tw`rounded-xl ml-5`}
+					></StyledNav>
+				</section>
+			</HeaderStyle>
 		</>
 	);
 };
 
+const HeaderStyle = styled.header(({ color }) => [
+	color,
+	tw`
+		h-32 inline-flex w-full text-6xl mx-auto
+		`,
+]);
+
+const StyledNav = styled.nav(({ color, rest }) => [
+	color,
+	tw`w-16 h-16 m-8`,
+	rest,
+]);
 export default Header;
