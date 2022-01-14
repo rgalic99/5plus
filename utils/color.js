f7e0fadbb48bc44626ab7d98218c7e298a855492
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-01-14 16:54:37 +0100

    Reformated color.js util file

--- utils/color.js
+++ utils/color.js
@@ -1,19 +1,17 @@
 import tw from "twin.macro";
 
-export const setBackgroundColor = (color = "main") =>
-	({
-		main: tw`bg-main`,
-		math: tw`bg-math_main`,
-		history: tw`bg-history_main`,
-		science: tw`bg-science_main`,
-		croatian: tw`bg-croatian_main`,
-	}[color]);
+export const BackgroundColor = {
+	main: tw`bg-main`,
+	math: tw`bg-math_main`,
+	history: tw`bg-history_main`,
+	science: tw`bg-science_main`,
+	croatian: tw`bg-croatian_main`,
+};
 
-export const setHighlightColor = (color = "main") =>
-	({
-		main: tw`text-second`,
-		math: tw`text-math_second`,
-		history: tw`text-history_second`,
-		science: tw`text-science_second`,
-		croatian: tw`text-croatian_second`,
-	}[color]);
+export const HighlightColor = {
+	main: tw`bg-second`,
+	math: tw`bg-math_second`,
+	history: tw`bg-history_second`,
+	science: tw`bg-science_second`,
+	croatian: tw`bg-croatian_second`,
+};
