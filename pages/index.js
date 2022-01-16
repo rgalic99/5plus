6ed903a432f60fa8b0b0f846caddbc79298847b1
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-01-16 16:50:30 +0100

    Updated home page

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,26 @@
 import Header from "../components/Header";
-import tw, { styled } from "twin.macro";
+import Link from "next/link";
+import Heading, { Heading2 } from "../components/Heading";
+import { MainButton, SecondButton } from "../utils/button";
 
 const Home = () => {
-	const heading_style = tw`text-center m-3 col-start-3 col-span-9 text-main drop-shadow-2xl`;
 	return (
 		<>
 			<Header />
-			<section className="grid grid-cols-12">
-				<section className="col-span-12 my-16 bg-off_white/50">
-					<h1 style={heading_style} className="text-5xl font-main">
-						PROŠIRI SVOJE ZNANJE DANAS!
-					</h1>
-					<h2 style={heading_style} className="text-3xl font-second">
-						Za one koji žele znati više
-					</h2>
+			<section className="grid grid-cols-12 m-auto">
+				<section className="my-4 bg-off_white/50 m-auto col-span-12">
+					<Heading text={"PROŠIRI SVOJE ZNANJE DANAS!"} />
+					<Heading2 text={"Za one koji žele znati više"} />
 				</section>
-				<StyledButton rest={tw`bg-second text-main`}>
-					PRIJAVA
-				</StyledButton>
-				<StyledButton rest={tw`bg-main text-white`}>
-					STVORI RAČUN
-				</StyledButton>
+				<Link href="/login" passHref>
+					{SecondButton("PRIJAVA")}
+				</Link>
+				<Link href="/register" passHref>
+					{MainButton("STVORI RAČUN")}
+				</Link>
 			</section>
 		</>
 	);
 };
-
-const StyledButton = styled.button(({ rest }) => [
-	tw`col-span-6 col-start-4 m-4 p-4 rounded-2xl text-4xl`,
-	rest,
-]);
 
 export default Home;
