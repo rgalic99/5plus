6dd4d71c4b945fa7217896325d91aa637d4a1c05
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-01-16 16:49:30 +0100

    Added register page

---
+++ pages/register.js
@@ -0,0 +1,21 @@
+import { RegisterForm } from "../components/Form";
+import Header from "../components/Header";
+import Heading from "../components/Heading";
+import { LinkTo } from "../utils/link";
+
+const Register = () => {
+	return (
+		<>
+			<Header />
+			<section className="grid grid-cols-12">
+				<section className="col-span-12 my-8">
+					<Heading text={"ZAPOČNI DANAS!"} />
+					<RegisterForm />
+					{LinkTo("/login", "Imaš račun? Prijavi se!")}
+				</section>
+			</section>
+		</>
+	);
+};
+
+export default Register;
