51a055b4caaf924f8b456296cded2fb34ace55ec
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-01-16 16:49:17 +0100

    Added login page

---
+++ pages/login.js
@@ -0,0 +1,21 @@
+import { LoginForm } from "../components/Form";
+import Header from "../components/Header";
+import Heading from "../components/Heading";
+import { LinkTo } from "../utils/link";
+
+const Login = () => {
+	return (
+		<>
+			<Header />
+			<section className="grid grid-cols-12">
+				<section className="col-span-12 my-8">
+					<Heading text={"DOBRODOŠLI NATRAG!"}></Heading>
+					<LoginForm />
+					{LinkTo("/register", "Nemaš račun? Klikni ovdje!")}
+				</section>
+			</section>
+		</>
+	);
+};
+
+export default Login;
