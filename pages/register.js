165becab2729b1c794c5250372f6cd0124d50028
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-02-17 00:05:11 +0100

    Reformated buttons

--- pages/register.js
+++ pages/register.js
@@ -2,6 +2,8 @@ import { RegisterForm } from "../components/Form";
 import Header from "../modules/Header";
 import Heading from "../components/Heading";
 import { LinkTo } from "../utils/link";
+import { MainButton } from "../utils/button";
+import Link from "next/link";
 
 const Register = () => {
 	return (
@@ -10,7 +12,12 @@ const Register = () => {
 			<section className="grid grid-cols-12">
 				<section className="col-span-12 my-8">
 					<Heading text={"ZAPOČNI DANAS!"} />
-					<RegisterForm />
+					<form className="grid grid-cols-12 col-span-12 my-8 text-center">
+						<RegisterForm />
+						<Link passHref href="/success">
+							{MainButton("STVORI RAČUN")}
+						</Link>
+					</form>
 					{LinkTo("/login", "Imaš račun? Prijavi se!")}
 				</section>
 			</section>
--- utils/button.js
+++ utils/button.js
@@ -1,14 +1,19 @@
 import tw, { styled } from "twin.macro";
 
-const StyledButton = styled.button(({ rest }) => [
+const StyledButton = styled.button(({ rest, stlye = tw`` }) => [
 	tw`col-span-10 col-start-2 m-4 p-4 rounded-2xl text-4xl sm:col-start-4 sm:col-span-6`,
 	rest,
+	stlye,
 ]);
 
-export const SecondButton = (text) => (
-	<StyledButton rest={tw`text-main bg-second`}>{text}</StyledButton>
+export const SecondButton = (text, stlye = tw``) => (
+	<StyledButton rest={tw`text-main bg-second`} style={stlye}>
+		{text}
+	</StyledButton>
 );
 
-export const MainButton = (text) => (
-	<StyledButton rest={tw`text-white bg-main`}>{text}</StyledButton>
+export const MainButton = (text, stlye = tw``) => (
+	<StyledButton rest={tw`text-white bg-main`} style={stlye}>
+		{text}
+	</StyledButton>
 );
