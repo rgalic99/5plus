5249db670b79ceace6af12297ea2551609ac2ce8
gbatin00 <gogeh1234@gmail.com>
2022-04-12 13:12:44 +0200

    Made changes in how firestore handles references

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,7 +1,9 @@
-import firebase from "firebase/app";
-import "firebase/auth";
+import { initializeApp } from "firebase/app";
 import "firebase/firestore";
 import "firebase/storage";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyAvr_-zcaQ6WP3TvSAXL_vLb2XzHlMxS50",
@@ -12,10 +14,14 @@ const firebaseConfig = {
 	appId: "1:914868329858:web:74d2d285dfdccad1b4839b",
 };
 
-if (!firebase.getApps().length) {
-	firebase.initializeApp(firebaseConfig);
-}
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+// onAuthStateChanged(auth, (user) => {
+// 	// Check for user status
+// });
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
-export const storage = firebase.storage();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const googleAuthProvider = new GoogleAuthProvider();
+//TODO add Facebook login
+//export const facebookAuthProvider = new FacebookAuthProvider();
