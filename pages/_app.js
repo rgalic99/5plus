57ac4d1144daa370f56b6db152910da2ccfbd2e4
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-06-27 11:37:10 +0200

    reimported useUserData

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import "../styles/globals.css";
 import Head from "next/head";
 import { UserContext } from "../utils/context";
-import useUserData from "../utils/hooks";
+import { useUserData } from "../utils/hooks";
 
 function MyApp({ Component, pageProps }) {
 	const userData = useUserData();
 	return (
 		<>
 			<Head>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
 			</Head>
 			<UserContext.Provider value={userData}>
 				<Component {...pageProps} />
