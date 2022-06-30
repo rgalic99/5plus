94f48d121b3b395ea61c96fb708e1f677dd4bb13
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-07-01 01:02:01 +0200

    Fixed broken link

--- pages/success.js
+++ pages/success.js
@@ -1,9 +1,8 @@
 import Image from "next/image";
-import Link from "next/link";
 import tw from "twin.macro";
 import { Heading2 } from "../components/Heading";
 import Header from "../modules/Header";
-import { MainButton } from "../utils/button";
+import { MainButtonLink } from "../utils/button";
 
 const AccountSuccess = () => {
 	return (
@@ -18,15 +17,12 @@ const AccountSuccess = () => {
 					className="rounded-2xl"
 				></Image>
 				<section className="my-4 bg-second/60 m-auto text-center rounded-md px-4">
-					<Heading2 text={"Uspješno ste stvorili račun!"} />
-					<Link
-						href="/subjects"
-						className="text-center px-8"
-						passHref
-					>
-						{MainButton("ZAPOČNI", tw`px-20 rounded-3xl`)}
-						{/*TODO: FIX THIS LINK*/}
-					</Link>
+					<Heading2 text="Uspješno ste stvorili račun!" />
+					<MainButtonLink
+						text="ZAPOČNI"
+						link="/subjects"
+						css={tw`px-8 text-black`}
+					/>
 				</section>
 			</section>
 		</>
