dc928b25361bf69842169bbde9194d05252bb378
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-07-01 00:01:07 +0200

    removed hooks.js

--- utils/hooks.js
+++
@@ -1,7 +0,0 @@
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "./firebase";
-
-export const useUserData = () => {
-	const [user] = useAuthState(auth);
-	return user;
-};
