651a3679c48c559791998e7b35856ee134cbf8f7
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-07-01 00:01:54 +0200

    rewrote login and signup forms

--- components/Form.js
+++ components/Form.js
@@ -1,44 +1,68 @@
-import tw from "twin.macro";
 import { useForm } from "react-hook-form";
 import { MainButton, MainButtonSubmit } from "../utils/button";
+import { useUserAuth } from "../utils/contextProvider";
+import { useEffect, useRef, useState } from "react";
+import {
+	errorStyle,
+	formStyle,
+	input_style,
+	invalidStyle,
+	name_style1,
+	name_style2,
+	shared,
+} from "../constants/Styles";
 
 export const RegisterForm = () => {
-	const shared = tw`text-main bg-second text-overflow[elipsis] rounded-3xl m-3 p-4 drop-shadow-2xl  text-3xl text-left`;
-	const input_style = tw`col-span-10 col-start-2 sm:col-start-4 sm:col-span-6 placeholder:text-main`;
-	const name_style1 = tw`col-span-5 col-start-2  sm:col-start-4 sm:col-span-3 placeholder:text-main`;
-	const name_style2 = tw`col-span-5 col-start-7  sm:col-start-7 sm:col-span-3 placeholder:text-main`;
+	const { signUp } = useUserAuth();
+	const [error, setError] = useState(null);
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
+		watch,
 	} = useForm();
-	const onSubmit = (data) => console.log(data);
-	const style =
-		"text-red-700 my-auto mr-2 text-center text-2xl font-['Exo 2'] sm:text-3xl mr-16";
+
+	const password = useRef({});
+	password.current = watch("password", "");
+
+	useEffect(() => {
+		if (errors?.password2) {
+			setError(errors.password2.message);
+		} else {
+			setError(null);
+		}
+	}, [errors.password2]);
+
+	const onSubmit = async (data) => {
+		const { email, password } = data;
+
+		try {
+			await signUp(email, password);
+		} catch (e) {
+			setError("Korisnik sa ovom email adresom vec postoji!");
+		}
+	};
 
 	return (
 		<>
-			<form
-				onSubmit={handleSubmit(onSubmit)}
-				className="grid grid-cols-12 col-span-12 my-8 text-center"
-			>
+			<form onSubmit={handleSubmit(onSubmit)} className={formStyle}>
 				<input
 					type="text"
 					placeholder="E-mail"
 					autoComplete="email"
-					{...register("Email", {
+					{...register("email", {
 						required: true,
 						pattern: /^\S+@\S+$/i,
 					})}
 					css={input_style}
 					style={shared}
 				/>
-				{errors.Email && <p className={style}>X</p>}
+				{errors.email && <p className={invalidStyle}>X</p>}
 				<input
 					type="text"
 					placeholder="Ime"
 					autoComplete="given-name"
-					{...register("Ime", { required: true, maxLength: 80 })}
+					{...register("name", { required: true, maxLength: 80 })}
 					css={name_style1}
 					style={shared}
 				/>
@@ -46,16 +70,18 @@ export const RegisterForm = () => {
 					type="text"
 					placeholder="Prezime"
 					autoComplete="family-name"
-					{...register("Prezime", { required: true, maxLength: 100 })}
+					{...register("lName", { required: true, maxLength: 100 })}
 					css={name_style2}
 					style={shared}
 				/>
-				{(errors.Ime || errors.Prezime) && <p className={style}>X</p>}
+				{(errors.name || errors.lName) && (
+					<p className={invalidStyle}>X</p>
+				)}
 				<input
 					type="password"
 					placeholder="Lozinka"
 					autoComplete="new-password"
-					{...register("Lozinka", {
+					{...register("password", {
 						required: true,
 						maxLength: 64,
 						pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i,
@@ -63,65 +89,63 @@ export const RegisterForm = () => {
 					css={input_style}
 					style={shared}
 				/>
-				{errors.Lozinka && <p className={style}>X</p>}
+				{errors.password && <p className={invalidStyle}>X</p>}
 				<input
 					type="password"
 					placeholder="Ponovljena lozinka"
 					autoComplete="new-password"
-					{...register("Ponovljena_lozinka", {
+					{...register("password2", {
 						required: true,
 						maxLength: 64,
 						pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i,
+						validate: (value) =>
+							value === password.current ||
+							"Lozinke se ne podudaraju!",
 					})}
 					css={input_style}
 					style={shared}
 				/>
-				{errors.Ponovljena_lozinka && <p className={style}>X</p>}
+				{errors.password2 && <p className={invalidStyle}>X</p>}
 				<MainButtonSubmit text="STVORI RAČUN" />
 			</form>
+			{error && <p className={errorStyle}>{error}</p>}
 		</>
 	);
 };
 
 export const LoginForm = () => {
-	const input_style = tw`text-main bg-second text-overflow[elipsis] col-span-10 col-start-2 rounded-3xl m-3 p-4 drop-shadow-2xl placeholder:text-main text-3xl text-left sm:col-start-4 sm:col-span-6`;
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
 	} = useForm();
 	const onSubmit = (data) => console.log(data);
-	const style =
-		"text-red-700 my-auto mr-2 text-center text-2xl font-['Exo 2'] sm:text-3xl mr-16 md: mr-32";
 
 	return (
-		<form
-			onSubmit={handleSubmit(onSubmit)}
-			className="grid grid-cols-12 col-span-12 my-8 text-center"
-		>
+		<form onSubmit={handleSubmit(onSubmit)} className={formStyle}>
 			<input
 				type="text"
 				placeholder="E-mail"
 				autoComplete="email"
-				{...register("Email", {
+				{...register("email", {
 					required: true,
 					pattern: /^\S+@\S+$/i,
 				})}
 				css={input_style}
 			/>
-			{errors.Email && <p className={style}>X</p>}
+			{errors.email && <p className={invalidStyle}>X</p>}
 			<input
 				type="password"
 				placeholder="Lozinka"
 				autoComplete="current-password"
-				{...register("Lozinka", {
+				{...register("password", {
 					required: true,
 					maxLength: 64,
 					pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i,
 				})}
 				css={input_style}
 			/>
-			{errors.Lozinka && <p className={style}>X</p>}
+			{errors.password && <p className={invalidStyle}>X</p>}
 
 			<MainButton text="PRIJAVA" />
 		</form>
