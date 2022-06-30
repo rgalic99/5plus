92c7ec9de69dd9dbba76bdb8b404c46945baf579
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-07-01 01:01:36 +0200

    added opacity to list items

--- components/SubjectCard.js
+++ components/SubjectCard.js
@@ -5,7 +5,7 @@ const SubjectCard = ({ name, link, color }) => {
 		<Link passHref href={`/${link}`}>
 			<button
 				style={color}
-				className="border-4 self-center p-5 rounded-2xl my-3 w-80 bg-white text-5xl"
+				className="border-4 self-center p-5 rounded-2xl my-3 w-80 bg-white bg-opacity-60 text-5xl hover:text-amber-700 cursor-pointer"
 			>
 				{name}
 			</button>
