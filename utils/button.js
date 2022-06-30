f0fd17a04918cf3f1948fc0ac969d3b16136875b
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-07-01 00:58:31 +0200

    fixed typo

--- utils/button.js
+++ utils/button.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import tw, { styled } from "twin.macro";
 
-const StyledButton = styled.button(({ rest, stlye = tw`` }) => [
+const StyledButton = styled.button(({ rest, style = tw`` }) => [
 	tw`col-span-10 col-start-2 m-4 rounded-2xl text-4xl sm:col-start-4 sm:col-span-6`,
 	rest,
-	stlye,
+	style,
 ]);
 
 const InputMain = styled.input.attrs(() => ({
@@ -19,37 +19,37 @@ const InputSecond = styled.input.attrs(() => ({
 	${tw`text-main bg-second p-4 col-span-10 col-start-2 m-4 rounded-2xl text-4xl sm:col-start-4 sm:col-span-6`}
 `;
 
-export const MainButton = ({ text, stlye = tw`` }) => {
+export const MainButton = ({ text, style = tw`` }) => {
 	return (
-		<StyledButton rest={tw`text-white bg-main p-4`} style={stlye}>
+		<StyledButton rest={tw`text-white bg-main p-4`} style={style}>
 			{text}
 		</StyledButton>
 	);
 };
 
-export const SecondButton = ({ text, stlye = tw`` }) => {
+export const SecondButton = ({ text, style = tw`` }) => {
 	return (
-		<StyledButton rest={tw`text-main bg-second p-4`} style={stlye}>
+		<StyledButton rest={tw`text-main bg-second p-4`} style={style}>
 			{text}
 		</StyledButton>
 	);
 };
 
-export const MainButtonSubmit = ({ text, stlye = tw`` }) => {
+export const MainButtonSubmit = ({ text, style = tw`` }) => {
 	return (
 		<>
-			<InputMain style={stlye} value={text} />
+			<InputMain style={style} value={text} />
 		</>
 	);
 };
 
-export const SecondButtonSubmit = ({ text, stlye = tw`` }) => {
-	return <InputSecond style={stlye} value={text} />;
+export const SecondButtonSubmit = ({ text, style = tw`` }) => {
+	return <InputSecond style={style} value={text} />;
 };
 
-export const MainButtonLink = ({ text, stlye = tw``, link }) => {
+export const MainButtonLink = ({ text, css = tw``, link }) => {
 	return (
-		<StyledButton rest={tw`text-white bg-main`} style={stlye}>
+		<StyledButton rest={tw`text-white p-4 bg-main`} style={css}>
 			<Link href={link}>
 				<a className="block w-full p-4">{text}</a>
 			</Link>
@@ -57,9 +57,9 @@ export const MainButtonLink = ({ text, stlye = tw``, link }) => {
 	);
 };
 
-export const SecondButtonLink = ({ text, stlye = tw``, link }) => {
+export const SecondButtonLink = ({ text, css = tw``, link }) => {
 	return (
-		<StyledButton rest={tw`text-main bg-second`} style={stlye}>
+		<StyledButton rest={tw`text-main p-4 bg-second`} css={css}>
 			<Link href={link}>
 				<a className="block w-full p-4">{text}</a>
 			</Link>
