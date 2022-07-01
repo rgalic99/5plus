7e244cefe052194128a00ad8969b6e21774ab63f
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-07-01 18:15:37 +0200

    added google sign in button to register screen

--- pages/register.js
+++ pages/register.js
@@ -2,6 +2,7 @@ import { RegisterForm } from "../components/Form";
 import Header from "../modules/Header";
 import Heading from "../components/Heading";
 import { LinkTo } from "../utils/link";
+import { SignInButtonGoogle } from "../utils/googleLogin";
 
 const Register = () => {
 	return (
@@ -11,6 +12,8 @@ const Register = () => {
 				<section className="col-span-12 my-8">
 					<Heading text={"ZAPOČNI DANAS!"} />
 					<RegisterForm />
+					<SignInButtonGoogle />
+					<br />
 					<LinkTo href="/login" text="Imaš račun? Prijavi se!" />
 				</section>
 			</section>
