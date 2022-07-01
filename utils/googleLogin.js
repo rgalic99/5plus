e0f85713159707e3edd66cf545069bb2fa3231d8
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-07-01 18:14:47 +0200

    added google login functionality

--- utils/googleLogin.js
+++ utils/googleLogin.js
@@ -1,18 +1,24 @@
 import Image from "next/image";
-import React from "react";
-import { auth, googleAuthProvider } from "./firebase";
-import { signInWithRedirect } from "firebase/auth";
+import { useRouter } from "next/router";
 import tw from "twin.macro";
+import { useUserAuth } from "./contextProvider";
 
 export const SignInButtonGoogle = () => {
-	const signInWithGoogle = async () => {
-		await signInWithRedirect(auth, googleAuthProvider);
+	const { googleSignIn } = useUserAuth();
+	const router = useRouter();
+	const handleGoogleSignIn = async (e) => {
+		e.preventDefault();
+		try {
+			await googleSignIn();
+			router.push("/subjects");
+		} catch (e) {
+			throw new Error(e);
+		}
 	};
-
 	return (
 		<button
 			className="flex items-center text-main justify-center m-auto text-3xl"
-			onClick={signInWithGoogle}
+			onClick={handleGoogleSignIn}
 			css={tw`p-4 border-gray-400 border-2 rounded-2xl`}
 		>
 			<Image
@@ -24,8 +30,4 @@ export const SignInButtonGoogle = () => {
 			<span className="ml-5">Prijavi se s Google računom!</span>
 		</button>
 	);
-};
-
-export const SignOutButton = () => {
-	return <button onClick={() => auth.signOut()}>Odjava</button>;
 };
