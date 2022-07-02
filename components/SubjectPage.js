7882f8b6b2f55bc9ff5d3b0a93c787872e2ec1d4
Roko Galić <66263242+rgalic99@users.noreply.github.com>
2022-07-02 19:10:04 +0200

    added general subject page

---
+++ components/SubjectPage.js
@@ -0,0 +1,28 @@
+import { namesToSubject } from "../constants/SubjectsList";
+import Footer from "../modules/Footer";
+import Header from "../modules/Header";
+import Protected from "./Protected";
+
+const SubjectPage = ({ subject }) => {
+	const { name, categories } = subject;
+	return (
+		<Protected>
+			<Header color={name} />
+			<h1>{namesToSubject[name]}</h1>
+			{categories.map((category) => (
+				<li key={category}>
+					<h2>{category.name}</h2>
+					<ul>
+						{category.questions.map((question, j) => (
+							<li key={question}>
+								<h3>{j + 1}</h3>
+							</li>
+						))}
+					</ul>
+				</li>
+			))}
+			<Footer />
+		</Protected>
+	);
+};
+export default SubjectPage;
