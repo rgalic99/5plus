ea8d4a22a82c208eff41a440607f71b5ab1b3530
gbatin00 <gogeh1234@gmail.com>
2022-07-03 12:11:28 +0200

    Changed some changes

--- components/Heading.js
+++ components/Heading.js
@@ -4,7 +4,7 @@ const Heading = ({ text }) => {
 	return (
 		<h1
 			css={tw`m-8 col-start-4 col-span-6 text-main drop-shadow-2xl`}
-			className="text-4xl font-main text-left lg:bg-transparent lg:text-8xl lg:text-right lg:items-center "
+			className="text-5xl font-main text-center"
 		>
 			{text}
 		</h1>
@@ -14,8 +14,8 @@ const Heading = ({ text }) => {
 export const Heading2 = ({ text }) => {
 	return (
 		<h2
-			css={tw`text-left m-8 col-start-3 col-span-9 text-main drop-shadow-2xl`}
-			className="text-2xl font-second lg:text-5xl lg:items-center"
+			css={tw`text-center m-8 col-start-3 col-span-9 text-main drop-shadow-2xl`}
+			className="text-3xl font-second"
 		>
 			{text}
 		</h2>
