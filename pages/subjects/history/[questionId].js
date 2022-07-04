0b564119a6c16f30a119a5bef5fad3d5f664a344
gbatin00 <gogeh1234@gmail.com>
2022-07-04 21:10:26 +0200

    added question pages

---
+++ pages/subjects/history/[questionId].js
@@ -0,0 +1,54 @@
+import Footer from "../../../modules/Footer";
+import Header from "../../../modules/Header";
+import { getQuestionsFromSubject } from "../../../utils/firebase";
+
+const QuestionPage = () => {
+	const pageColor = "history";
+	return (
+		<>
+			<Header color={pageColor} />
+			<main className="w-2/3 mx-auto my-0 flex flex-col items-center">
+				<h1 className="text-center text-8xl text-main"></h1>
+				<h2 className="text-center text-5xl my-8 text-gray-600">
+					Some subtitle can go here
+				</h2>
+
+				<p className="text-gray-800 text-xl mt-16">...</p>
+				<p>Read more bellow</p>
+			</main>
+			<Footer color={pageColor} />
+		</>
+	);
+};
+
+export default QuestionPage;
+
+const getPostBySlug = async (questionId) => {
+	return await getQuestionsFromSubject("history").then((questions) =>
+		questions.find((question) => question.id === questionId)
+	);
+};
+
+export async function getStaticProps({ params: { questionId } }) {
+	const question = await getPostBySlug(questionId);
+
+	return {
+		props: {
+			post: null,
+		},
+	};
+}
+
+export async function getStaticPaths() {
+	const questions = await getQuestionsFromSubject("history");
+	const paths = questions.flat().map((question) => ({
+		params: {
+			questionId: question.id,
+		},
+	}));
+
+	return {
+		paths,
+		fallback: false,
+	};
+}
